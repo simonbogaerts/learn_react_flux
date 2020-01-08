188c08d18c13888c37622e6884ce586a5e7b9e4f
simonbogaerts <simon.bogaerts@outlook.com>
2020-01-08 15:09:46 +0100

    forms

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,19 @@ import CoursesPage from "./CoursesPage";
 import ManageCoursePage from "./ManageCoursePage";
 import NotFoundPage from "./NotFoundPage";
 import { Route, Switch, Redirect } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <div className="container-fluid">
+      <ToastContainer autoClose={3000} hideProgressBar />
       <Header />
       <Switch>
         <Route path="/" component={HomePage} exact />
         <Route path="/courses" component={CoursesPage} />
         <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route path="/course" component={ManageCoursePage} />
         <Route path="/about" component={AboutPage} />
         <Redirect from="/about-page" to="/about" />
         <Route component={NotFoundPage} />
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
+import { toast } from "react-toastify";
 
 const ManageCoursePage = props => {
+  const [errors, setErrors] = useState({});
+
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: ""
+  });
+
+  useEffect(() => {
+    const slug = props.match.params.slug;
+
+    if (slug) {
+      courseApi.getCourseBySlug(slug).then(_course => setCourse(_course));
+    }
+  }, [props.match.params.slug]);
+
+  function handleChange({ target }) {
+    const updatedCourse = {
+      ...course,
+      [target.name]: target.value
+    };
+    setCourse(updatedCourse);
+  }
+
+  function formIsValid() {
+    const _errors = {};
+
+    if (!course.title) {
+      _errors.title = "Title is required.";
+    }
+
+    if (!course.authorId) {
+      _errors.author = "Author ID is required.";
+    }
+
+    if (!course.category) {
+      _errors.category = "Category is required.";
+    }
+
+    setErrors(_errors);
+
+    return Object.keys(_errors).length === 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!formIsValid()) {
+      return;
+    }
+
+    courseApi.saveCourse(course).then(() => {
+      props.history.push("/courses");
+      toast.success("Course saved.");
+    });
+  }
+
   return (
     <>
       <h2>Manage Courses</h2>
-      {props.match.params.slug}
+      <CourseForm
+        course={course}
+        errors={errors}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </>
   );
 };
